f5432768a66e02c883b171eff1c8c4c1cebeeef8
David Anastasov <davidanastasov@hotmail.com>
2025-03-09 01:40:20 +0100

    Initialize empty project

---
+++ src/main/java/com/davidanastasov/emtlabproject/EmtLabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.davidanastasov.emtlabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtLabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmtLabProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davidanastasov/emtlabproject/EmtLabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidanastasov.emtlabproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtLabProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
