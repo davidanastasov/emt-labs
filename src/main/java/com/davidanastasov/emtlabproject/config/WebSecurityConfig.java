c24bdc6ca2c0a2680a8501f3691e46057e5c72b2
David Anastasov <davidanastasov@hotmail.com>
2025-03-10 02:25:08 +0100

    Disable CSRF protection

--- src/main/java/com/davidanastasov/emtlabproject/config/WebSecurityConfig.java
+++ src/main/java/com/davidanastasov/emtlabproject/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,6 +27,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(HttpMethod.GET, "api/**").permitAll()
                         .requestMatchers("/api/books/**").hasAnyRole(Role.ADMIN.getAuthority(), Role.LIBRARIAN.getAuthority())
