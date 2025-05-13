8c76dd6eff66219cbdd7921c7a44beb3b823e849
David Anastasov <davidanastasov@hotmail.com>
2025-05-13 03:10:36 +0200

    Redesign app layout

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,4 +1,6 @@
-import { Link } from "react-router";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { BookOpen } from "lucide-react";
+import { Link, NavLink } from "react-router";
 
 interface NavLink {
   path: string;
@@ -14,18 +16,43 @@ const navLinks: NavLink[] = [
 
 export default function Header() {
   return (
-    <header>
-      <h3>Book Library</h3>
+    <AppBar component="nav" position="static">
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          sx={{
+            flexGrow: 1,
+            color: "white",
+            textDecoration: "none",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <BookOpen size={24} style={{ marginRight: "8px" }} />
+          Book Library
+        </Typography>
 
-      <nav>
-        <ul>
+        <nav>
           {navLinks.map((navLink) => (
-            <Link key={navLink.label} to={navLink.path}>
-              <li>{navLink.label}</li>
-            </Link>
+            <Button
+              key={navLink.label}
+              component={NavLink}
+              to={navLink.path}
+              sx={{
+                color: "#fff",
+                mx: 1,
+                "&.active": {
+                  bgcolor: "rgba(255, 255, 255, 0.1)",
+                },
+              }}
+            >
+              {navLink.label}
+            </Button>
           ))}
-        </ul>
-      </nav>
-    </header>
+        </nav>
+      </Toolbar>
+    </AppBar>
   );
 }
--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,14 +1,28 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
+import { Box, Container } from "@mui/material";
 
 export default function Layout() {
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       <Header />
 
-      <main>
+      <Container
+        component="main"
+        maxWidth="lg"
+        sx={{
+          flexGrow: 1,
+          py: 2,
+        }}
+      >
         <Outlet />
-      </main>
-    </>
+      </Container>
+    </Box>
   );
 }
